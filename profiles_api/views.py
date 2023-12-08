19f163656d7837134f2c8c3131d43b4c0f0a6b51
msanjith130@gmail.com <148097204+sanjithhithub@users.noreply.github.com>
2023-12-08 19:45:20 +0530

    added the root api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
 from profiles_api import serializers
 
 class HelloApiView(APIView):
@@ -43,3 +44,16 @@ class HelloApiView(APIView):
     def delete(self, request, pk=None):
         """Delete an object"""
         return Response({'method':'DELETE'})
+
+class HelloViewSets(viewsets.ViewSet):
+    """Test ApI ViewSets"""
+
+    def list(self, request):
+        """Return a hello message"""
+
+        a_viewsets = [
+          'User actions (list,create,retrieve,update,partial_update),'
+          ''
+
+
+        ]
