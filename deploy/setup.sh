46ecd0c57776733f06b363b44a11ff700fa709f3
msanjith130@gmail.com <148097204+sanjithhithub@users.noreply.github.com>
2023-12-15 18:46:37 +0530

    yum added

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -8,8 +8,8 @@ PROJECT_GIT_URL='https://github.com/sanjithhithub/profile-rest-api.git'
 PROJECT_BASE_PATH='C:\Users\sanjith\Desktop\New folder\profile-rest-api>'  
 
 echo "Installing dependencies..."
-apt-get update
-apt-get install -y python3-dev python3-venv sqlite python-pip supervisor nginx git
+apt-yum update
+apt-yum install -y python3-dev python3-venv sqlite python-pip supervisor nginx git
 
 # Create project directory
 mkdir -p $PROJECT_BASE_PATH
