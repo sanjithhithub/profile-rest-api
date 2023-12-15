e025c65c309e386825f294da4b8c208178281541
msanjith130@gmail.com <148097204+sanjithhithub@users.noreply.github.com>
2023-12-15 19:31:55 +0530

    added new one

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -3,7 +3,7 @@
 set -e
 
 # TODO: Set to URL of git repo.
-PROJECT_GIT_URL='https://github.com/CHANGEME.git'
+PROJECT_GIT_URL='https://github.com/sanjithhithub/profile-rest-api.git'
 
 PROJECT_BASE_PATH='C:\Users\sanjith\Desktop\New folder\profile-rest-api'
 
