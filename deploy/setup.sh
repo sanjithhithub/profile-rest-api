65490d35127260fe11abf2c22a97939112fa5ab7
msanjith130@gmail.com <148097204+sanjithhithub@users.noreply.github.com>
2023-12-15 19:55:34 +0530

    added

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -1,15 +1,15 @@
-#!/C:\Users\sanjith\Desktop\New folder\profile-rest-api
+#!/usr/bin/env bash
 
 set -e
 
 # TODO: Set to URL of git repo.
-PROJECT_GIT_URL='https://github.com/sanjithhithub/profile-rest-api.git'
+PROJECT_GIT_URL='https://github.com/CHANGEME.git'
 
-PROJECT_BASE_PATH='C:\Users\sanjith\Desktop\New folder\profile-rest-api'  
+PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
 
 echo "Installing dependencies..."
-yum update
-yum install -y python python-venv sqlite python-pip supervisor nginx git
+sudo-yum update
+sudo-yum install -y python3-dev python3-venv sqlite python-pip supervisor nginx git
 
 # Create project directory
 mkdir -p $PROJECT_BASE_PATH
@@ -19,15 +19,9 @@ git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH
 mkdir -p $PROJECT_BASE_PATH/env
 python3 -m venv $PROJECT_BASE_PATH/env
 
-# Activate virtual environment
-source $PROJECT_BASE_PATH/env/bin/activate
-
 # Install python packages
-pip install -r $PROJECT_BASE_PATH/requirements.txt
-pip install uwsgi==2.0.18
-
-# Deactivate virtual environment
-deactivate
+$PROJECT_BASE_PATH/env/bin/pip install -r $PROJECT_BASE_PATH/requirements.txt
+$PROJECT_BASE_PATH/env/bin/pip install uwsgi==2.0.18
 
 # Run migrations and collectstatic
 cd $PROJECT_BASE_PATH
@@ -35,14 +29,15 @@ $PROJECT_BASE_PATH/env/bin/python manage.py migrate
 $PROJECT_BASE_PATH/env/bin/python manage.py collectstatic --noinput
 
 # Configure supervisor
-cp $PROJECT_BASE_PATH/deploy/supervisor_profiles_api.conf /etc/supervisord.d/profiles_api.conf
-supervisord
+cp $PROJECT_BASE_PATH/deploy/supervisor_profiles_api.conf /etc/supervisor/conf.d/profiles_api.conf
 supervisorctl reread
 supervisorctl update
 supervisorctl restart profiles_api
 
 # Configure nginx
-cp $PROJECT_BASE_PATH/deploy/nginx_profiles_api.conf /etc/nginx/conf.d/profiles_api.conf
-systemctl restart nginx
+cp $PROJECT_BASE_PATH/deploy/nginx_profiles_api.conf /etc/nginx/sites-available/profiles_api.conf
+rm /etc/nginx/sites-enabled/default
+ln -s /etc/nginx/sites-available/profiles_api.conf /etc/nginx/sites-enabled/profiles_api.conf
+systemctl restart nginx.service
 
 echo "DONE! :)"
