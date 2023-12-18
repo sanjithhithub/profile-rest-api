6b81981e70629514c381aeac0c47ea29d9b460d8
msanjith130@gmail.com <148097204+sanjithhithub@users.noreply.github.com>
2023-12-18 13:49:06 +0530

    added the path

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -4,7 +4,7 @@ set -e
 
 # TODO: Set to URL of git repo.
 PROJECT_GIT_URL='https://github.com/sanjithhithub/profile-rest-api.git'
-PROJECT_BASE_PATH='/path/to/your/preferred/location/profiles-rest-api'
+PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
 
 echo "Installing dependencies..."
 sudo yum update
