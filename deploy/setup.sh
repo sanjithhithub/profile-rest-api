53354daa3e36ceb49acb277e0de766987ce9de80
msanjith130@gmail.com <148097204+sanjithhithub@users.noreply.github.com>
2023-12-18 20:36:49 +0530

    added

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -21,7 +21,8 @@ python3 -m venv $PROJECT_BASE_PATH/env
 
 # Install python packages
 $PROJECT_BASE_PATH/env/bin/pip install -r $PROJECT_BASE_PATH/requirements.txt
-$PROJECT_BASE_PATH/env/bin/pip install gunicorn==19.9.0
+$PROJECT_BASE_PATH/env/bin/pip install wsgi==19.9.0
+
 # Run migrations and collectstatic
 cd $PROJECT_BASE_PATH
 $PROJECT_BASE_PATH/env/bin/python manage.py migrate
