98a62638ca19948b12e2ac68bd0e0c1529bea8e0
msanjith130@gmail.com <148097204+sanjithhithub@users.noreply.github.com>
2023-12-18 14:26:15 +0530

    remove the comma

--- deploy/update.sh
+++ deploy/update.sh
@@ -3,7 +3,7 @@
 
 set -e
 
-PROJECT_BASE_PATH=''\Users\sanjith\Desktop\New'  '\env'  'folder\profile-rest-api''
+PROJECT_BASE_PATH='\Users\sanjith\Desktop\New'  '\env'  'folder\profile-rest-api'
 
 
 git pull
