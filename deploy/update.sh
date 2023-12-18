235fe51415ff02232c25f09a1bb5a5742c3d30f6
msanjith130@gmail.com <148097204+sanjithhithub@users.noreply.github.com>
2023-12-18 13:10:28 +0530

    added the new file

---
+++ deploy/update.sh
@@ -0,0 +1,14 @@
+#!#!/usr/bin/env bash
+
+
+set -e
+
+PROJECT_BASE_PATH='/path/to/your/preferred/location/profiles-rest-api'
+
+
+git pull
+$PROJECT_BASE_PATH/env/bin/python manage.py migrate
+$PROJECT_BASE_PATH/env/bin/python manage.py collectstatic --noinput
+supervisorctl restart profiles_api
+
+echo "DONE! :)"
