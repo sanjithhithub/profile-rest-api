018852047464ebbbf1ba4b530b4aed590e398041
msanjith130@gmail.com <148097204+sanjithhithub@users.noreply.github.com>
2023-12-18 13:46:12 +0530

    remove the python

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -8,6 +8,5 @@ PROJECT_BASE_PATH='/path/to/your/preferred/location/profiles-rest-api'
 
 echo "Installing dependencies..."
 sudo yum update
-sudo yum install -y python3-dev python3-venv sqlite python3-pip supervisor nginx git
 
 # The rest of your script remains unchanged
