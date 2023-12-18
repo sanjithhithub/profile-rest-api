ed57d9ae03cc580c6548765d1a5229425812f950
msanjith130@gmail.com <148097204+sanjithhithub@users.noreply.github.com>
2023-12-18 13:57:37 +0530

    remove s

--- deploy/update.sh
+++ deploy/update.sh
@@ -3,12 +3,12 @@
 
 set -e
 
-PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
+PROJECT_BASE_PATH='/usr/local/apps/profile-rest-api'
 
 
 git pull
 $PROJECT_BASE_PATH/env/bin/python manage.py migrate
 $PROJECT_BASE_PATH/env/bin/python manage.py collectstatic --noinput
-supervisorctl restart profiles_api
+supervisorctl restart profile_api
 
 echo "DONE! :)"
